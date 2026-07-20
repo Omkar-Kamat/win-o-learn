e196e241a246f12f8bec317d06b8f10d3cb360ba
Omkar-Kamat <mail4omkarkamat@gmail.com>
2026-07-20 12:12:49 +0530

    feat: configure MongoDB connection

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,11 @@
+import "dotenv/config";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
